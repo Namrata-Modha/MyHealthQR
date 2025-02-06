6b7ada086d4d799675e98bf85076d8b53afd7406
Namrata Modha <Modha.s.namrata@gmail.com>
2025-02-06 14:53:56 -0500

    Trying to fix health route issue

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/healthz', function () {
+
+Route::get('/healthz', function (Request $request) {
     return response()->json(['status' => 'ok'], 200);
 });
